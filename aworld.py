d5d8934c22317959880f4abc1b3c24f7b5874817
bharathk005 <bharathk005@gmail.com>
2019-03-29 00:36:56 +0530

    changes to NN

--- aworld.py
+++ aworld.py
@@ -9,151 +9,157 @@ import pymunk.pygame_util
 import numpy as np1
 
 class CreateWorld(object):
-    def __init__(self):
-        self.displayX = 600
-        self.displayY = 600
-  
+	def __init__(self):
+		self.displayX = 600
+		self.displayY = 600
+  		self.action_size = 2
+  		self.action_space = [-1 , 1]
+		self.space = pymunk.Space()
+		self.space.gravity = (0.0,-986)
 
-        self.space = pymunk.Space()
-        self.space.gravity = (0.0,-986)
+		self.physicsStepsPerFrame = 1
+		self.dt = 1.0/60.0
+		self.force_dir = 1
+		self.force_mag = 5
+		self.objects = []
+		self.running = True
+		self.box_dir = 0
+		self.n_dir =1
+		self.left_wall = (100.0,100.0)
+		self.right_wall = (500.0,100.0)
+		self.start_pos = self.left_wall
+		self.x_prev = self.start_pos[0]
+		self.staticScene()
+		self.createBox()
+		self.init_pygame()
 
-        self.physicsStepsPerFrame = 1
-        self.dt = 1.0/60.0
-        self.force_dir = 1
-        self.force_mag = 5
-        self.objects = []
-        self.running = True
-        self.box_dir = 0
-        self.n_dir =1
-        self.left_wall = (100.0,100.0)
-        self.right_wall = (500.0,100.0)
-        self.start_pos = self.left_wall
-        self.x_prev = self.start_pos[0]
-        self.staticScene()
-        self.createBox()
-        self.init_pygame()
+	def process_frame(self,screen):
+		window_dat = pygame.surfarray.array2d(screen)
+		offset = 20
+		window_dat = window_dat[int(self.left_wall[0]-offset):int(self.right_wall[0]+offset),int(self.displayY-self.left_wall[1]-offset):int(self.displayY-self.right_wall[1]+offset)]
+		window_dat = window_dat.reshape((1,window_dat.shape[0]*window_dat.shape[1]))
+		window_dat -= window_dat.min()
+		window_dat = window_dat/window_dat.max()
+		state = np1.append(window_dat,self.box_dir)
+		return state
 
-    def process_frame(self,screen):
-        window_dat = pygame.surfarray.array2d(screen)
-        offset = 20
-        window_dat = window_dat[int(self.left_wall[0]-offset):int(self.right_wall[0]+offset),int(self.displayY-self.left_wall[1]-offset):int(self.displayY-self.right_wall[1]+offset)]
-        window_dat = window_dat.reshape((1,window_dat.shape[0]*window_dat.shape[1]))
-        window_dat -= window_dat.min()
-        window_dat = window_dat/window_dat.max()
-        state = np1.append(window_dat,self.box_dir)
-        print(state.shape)
-        return state
+	def init_pygame(self):
+		pygame.init()
+		self.screen = pygame.display.set_mode((self.displayX, self.displayY))
+		self.drawOption = pymunk.pygame_util.DrawOptions(self.screen)
 
-    def init_pygame(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((self.displayX, self.displayY))
-        self.clock = pygame.time.Clock()
-        self.drawOption = pymunk.pygame_util.DrawOptions(self.screen)
-        
+	def reset(self):
+		self.__init__()
+		self.clear_screen()
+		self.draw_objects()
+		state = self.process_frame(self.screen)
+		return state
 
-    def run_frame(self):       
-        for x in range(self.physicsStepsPerFrame):
-            self.space.step(self.dt)
-        velx,vely = self.objects[0].body.velocity_at_local_point((0,0))
-        self.milestones()
-        self.applyForce()
-        x,y = self.objects[0].body.position
+	def run_frame(self, action):       
+		for x in range(self.physicsStepsPerFrame):
+			self.space.step(self.dt)
+		velx,vely = self.objects[0].body.velocity_at_local_point((0,0))
+		self.milestones()
+		self.applyForce(action)
+		x,y = self.objects[0].body.position
 
-        x = round(x,4)
-        
-        if x - self.x_prev > 0:
-            self.box_dir =1
-        elif x - self.x_prev == 0:
-            self.box_dir = 0
-        else:
-         self.box_dir = -1 
-        reward = self.getReward()
-        
-        self.clear_screen()
-        self.draw_objects()
-        font = pygame.font.SysFont("Arial", 16)
-        self.screen.blit(font.render("fps: " + str(self.clock.get_fps()), 1, THECOLORS["black"]), (0,0))
-        self.screen.blit(font.render("force_dir: " + str(self.force_dir), 1, THECOLORS["black"]), (0,15))
-        self.screen.blit(font.render("n_dir: " + str(self.n_dir), 1, THECOLORS["black"]), (0,30))
-        self.screen.blit(font.render("Velocity: " + str(velx), 1, THECOLORS["black"]), (0,45))
+		x = round(x,4)
+		
+		if x - self.x_prev > 0:
+			self.box_dir =1
+		elif x - self.x_prev == 0:
+			self.box_dir = 0
+		else:
+		 self.box_dir = -1 
+		reward = self.getReward()
+		
+		self.clear_screen()
+		self.draw_objects()
+		font = pygame.font.SysFont("Arial", 16)
+		self.clock = pygame.time.Clock()
+		self.screen.blit(font.render("fps: " + str(self.clock.get_fps()), 1, THECOLORS["black"]), (0,0))
+		self.screen.blit(font.render("force_dir: " + str(self.force_dir), 1, THECOLORS["black"]), (0,15))
+		self.screen.blit(font.render("n_dir: " + str(self.n_dir), 1, THECOLORS["black"]), (0,30))
+		self.screen.blit(font.render("Velocity: " + str(velx), 1, THECOLORS["black"]), (0,45))
 
-        state = self.process_frame(self.screen)
-        pygame.display.flip()
+		state = self.process_frame(self.screen)
+		pygame.display.flip()
 
-        self.terminate_action()
-        # Delay fixed time between frames
-        #self.clock.tick(60)
-        return reward,state
-            
+		self.terminate_action()
+		# Delay fixed time between frames
+		#self.clock.tick(60)
+		return reward,state,self.running
+			
 
-    def staticScene(self):
-        staticBody = self.space.static_body
-        ground = pymunk.Segment(staticBody,self.left_wall, self.right_wall, 1.5)
-        ground.friction = 0.6
-        self.space.add(ground)
+	def staticScene(self):
+		staticBody = self.space.static_body
+		ground = pymunk.Segment(staticBody,self.left_wall, self.right_wall, 1.5)
+		ground.friction = 0.6
+		self.space.add(ground)
 
-    def createBox(self):
-        mass = 1
-        vertices = (10.0,10.0)
-        inertia = pymunk.moment_for_box(mass,(10.0,10.0))
-        body = pymunk.Body(mass,inertia)
-        body.position = self.start_pos
-        shape = pymunk.Poly.create_box(body,vertices)
-        shape.elasticity = 0.95
-        shape.friction = 0.5
-        self.space.add(body,shape)
-        self.objects.append(shape)
+	def createBox(self):
+		mass = 1
+		vertices = (10.0,10.0)
+		inertia = pymunk.moment_for_box(mass,(10.0,10.0))
+		body = pymunk.Body(mass,inertia)
+		body.position = self.start_pos
+		shape = pymunk.Poly.create_box(body,vertices)
+		shape.elasticity = 0.95
+		shape.friction = 0.5
+		self.space.add(body,shape)
+		self.objects.append(shape)
 
-    def applyForce(self):
-        obj = self.objects[0]
-        obj.body.apply_impulse_at_local_point((self.force_mag*self.force_dir,0),(0,0))
+	def applyForce(self,action):
+		obj = self.objects[0]
+		obj.body.apply_impulse_at_local_point((self.force_mag*self.action_space[action],0),(0,0))
 
-    def clear_screen(self):
-        
-        self.screen.fill(THECOLORS["white"])
+	def clear_screen(self):
+		
+		self.screen.fill(THECOLORS["white"])
 
-    def draw_objects(self):
- 
-        self.space.debug_draw(self.drawOption)
+	def draw_objects(self):
+		self.space.debug_draw(self.drawOption)
 
-    def terminate_action(self):
-        x,y = self.objects[0].body.position
-        if(y < 100.0 ):
-            self.running = False
+	def terminate_action(self):
+		x,y = self.objects[0].body.position
+		if(y < 100.0 ):
+			self.running = False
+		if self.n_dir == -1 and x <= self.left_wall[0] + 200:
+			self.running = False
 
-    def milestones(self):
-        x,y = self.objects[0].body.position
-        if x >= self.right_wall[0] - 10:
-            self.n_dir = -1
-        elif x<= self.left_wall[0] +10:
-            self.n_dir = 1
+	def milestones(self):
+		x,y = self.objects[0].body.position
+		if x >= self.right_wall[0] - 10:
+			self.n_dir = -1
+		elif x<= self.left_wall[0] +10:
+			self.n_dir = 1
 
-    def getReward(self):
-        reward = 0
-        x,y = self.objects[0].body.position
-        x = round(x,4)
-        y = round(y,4)
-        if self.n_dir == 1:
-            if self.x_prev < x:
-                reward = 1 
-            elif self.x_prev == x:
-                reward = 0
-            else:
-                reward = -1
-        elif self.n_dir == -1:
-            if self.x_prev > x:
-                reward = 1 
-            elif self.x_prev == x:
-                reward = 0
-            else:
-                reward = -1
-        self.x_prev = x
-        return reward
+	def getReward(self):
+		reward = 0
+		x,y = self.objects[0].body.position
+		x = round(x,4)
+		y = round(y,4)
+		if self.n_dir == 1:
+			if self.x_prev < x:
+				reward = 1 
+			elif self.x_prev == x:
+				reward = 0
+			else:
+				reward = -1
+		elif self.n_dir == -1:
+			if self.x_prev > x:
+				reward = 1 
+			elif self.x_prev == x:
+				reward = 0
+			else:
+				reward = -1
+		self.x_prev = x
+		return reward
 
 
 if __name__ == '__main__':
-    game = CreateWorld()
-    while game.running:
-        game.run_frame()
+	game = CreateWorld()
+	while game.running:
+		game.run_frame(1)
 
-    
+	
--- controlNN.py
+++ controlNN.py
@@ -6,16 +6,23 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-
+TOTAL_EPISODES = 50
+LR = 0.01
+GAMMA = 0.95
 
 def basic_control():
 	world = w.CreateWorld()
-	while world.running:
+	done = True
+	action = 1
+	while done:
 		
-		e.see_amoeba_world(0,50,world.displayX,world.displayY)
-		reward,state = world.run_frame()
+		#e.see_amoeba_world(0,50,world.displayX,world.displayY)
+		reward,state,done = world.run_frame(action)
 		x,y = world.objects[0].body.position
-		world.force_dir = world.n_dir 
+		action = int((world.n_dir +1)/2)
+		if world.running == False:
+			window_dat = world.reset()
+			print(window_dat.shape)
 		
 
 		# temp = pygame.display.set_mode((world.displayX, world.displayY))
@@ -24,7 +31,18 @@ def basic_control():
 		# plt.imshow(window_dat)
 		# plt.show()
 
-
+def discount_and_normalize_rewards(episode_rewards):
+    discounted_episode_rewards = np.zeros_like(episode_rewards)
+    cumulative = 0.0
+    for i in reversed(range(len(episode_rewards))):
+        cumulative = cumulative * GAMMA + episode_rewards[i]
+        discounted_episode_rewards[i] = cumulative
+    
+    mean = np.mean(discounted_episode_rewards)
+    std = np.std(discounted_episode_rewards)
+    discounted_episode_rewards = (discounted_episode_rewards - mean) / (std)
+    
+    return discounted_episode_rewards
 
 class CreateNN():
 	def __init__(self,ip_size,action_size,LR,name):
@@ -41,53 +59,84 @@ class CreateNN():
 			self.mean_reward = tf.placeholder(tf.float32,name = "mean_reward")
 
 			with tf.name_scope(name + "FC1"):
-				fc1 = tf.contrib.layers.fully_connected(input = self.inputs,
+				self.fc1 = tf.contrib.layers.fully_connected(input = self.inputs,
 					                                     num_outputs = self.ip_size * 2,
 					                                     activation_fn = tf.nn.relu,
 					                                     weights_initializer = tf.contrib.layers.xavier_initialer())
 			with tf.name_scope(name + "FC2"):
-				fc2 = tf.contrib.layers.fully_connected(input = fc1,
+				self.fc2 = tf.contrib.layers.fully_connected(input = self.fc1,
 					                                     num_outputs = self.ip_size * 2,
 					                                     activation_fn = tf.nn.relu,
 					                                     weights_initializer = tf.contrib.layers.xavier_initialer())
 
 			with tf.name_scope(name + "FC3"):
-				fc3 = tf.contrib.layers.fully_connected(input = fc2,
+				self.fc3 = tf.contrib.layers.fully_connected(input = self.fc2,
 					                                     num_outputs = self.action_size,
 					                                     activation_fn = tf.nn.relu,
 					                                     weights_initializer = tf.contrib.layers.xavier_initialer())
 
 			with tf.name_scope(name + "FC4"):
-				fc4 = tf.contrib.layers.fully_connected(input = fc3,
+				self.fc4 = tf.contrib.layers.fully_connected(input = self.fc3,
 					                                     num_outputs = self.action_size,
 					                                     activation_fn = None,
 					                                     weights_initializer = tf.contrib.layers.xavier_initialer())
 
 			with tf.name_scope(name + "softmax"):
-				action_prob = tf.nn.softmax(fc4)
+				self.action_prob = tf.nn.softmax(self.fc4)
 
 			with tf.name_scope(name + "loss"):
-				prob_log = tf.nn.softmax_cross_entropy_with_logits(logits = fc4,labels = self.actions)
-				loss = tf.reduce_mean(prob_log * self.disc_ep_reward)
+				self.prob_log = tf.nn.softmax_cross_entropy_with_logits(logits = self.fc4,labels = self.actions)
+				self.loss = tf.reduce_mean(self.prob_log * self.disc_ep_reward)
 
 			with tf.name_scope(name + "train"):
-				train_op = tf.train.AdamOptimizer(self.LR).minimize(loss)
+				self.train_op = tf.train.AdamOptimizer(self.LR).minimize(self.loss)
 
 
 
 
 def trainNN():
-	network = CreateNN()
-	rewards_vec = []
+	world = w.CreateWorld()
+	state = world.reset()
+
+	network = CreateNN(state.shape,world.action_size,LR,"FirstNN")
+	complete_rewards = []
 	total_rewards = 0
 	max_reward = 0
 	episode_states, episode_actions,episode_rewards = [],[],[]
 	with tf.Session() as sess:
 		sess.run(tf.global_vaiables_initializer())
-		world = w.CreateWorld()
+		
 
 		for episode in range(TOTAL_EPISODES):
 			reward_for_episode = 0
+			state = world.reset()
+
+			while True:
+				actions = sess.run(network.action_prob, feed_dict = {network.inputs:state})
+				choice = np.argmax(actions)
+				action = world.action_space[int(choice)]
+				reward,next_state,done = world.run_frame(action)
+
+				episode_rewards.append(reward)
+				episode_states.append(state)
+				episode_actions.append(actions)
+				if done:
+					reward_for_episode = np.sum(episode_rewards)
+					complete_rewards.append(reward_for_episode)
+
+					max_reward = np.amax(complete_rewards)
+
+					print("******************************")
+					print("Epi: ", episode)
+					print("Epi Reward: ", reward_for_episode)
+					print("Max Reward: ", max_reward)
+					dis_ep_reward = discount_and_normalize_rewards(episode_rewards)
+					loss,_ = sess.run([network.loss,network.train_op],feed_dict={network.inputs: np.vstack(np.array(episode_states)),network.actions: np.vstack(np.array(episode_actions)),network.disc_ep_reward: dis_ep_reward})
+
+					episode_states, episode_actions,episode_rewards = [],[],[]
+					break
+
+				state = next_state
 
 
 
